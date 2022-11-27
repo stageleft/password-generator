af6e30a6601231240cb5abddaa30babf1f630381
stageleft <sugano-github@elder-alliance.org>
2022-11-27 11:07:29 +0900

    calc_random unit test : replace to mock.

--- __test__/calc_random.password-generator.test.js
+++ __test__/calc_random.password-generator.test.js
@@ -1,29 +1,28 @@
 const pwgenerator = require("../src/password-generator.js");
-const crypto = require('node:crypto');
-
-let password_generator = new pwgenerator(crypto.webcrypto);
-let i; // loop value
+const mock = require("../__test__/test-mock.js");
 
 // module test
 test('test of calc_random(0)', ()=>{
+    const mock_webcrypto = new mock([[0],[255]]);
+    const password_generator = new pwgenerator(mock_webcrypto);
+    expect(password_generator.calc_random(0)).toBe(0)
     expect(password_generator.calc_random(0)).toBe(0)
 });
 test('test of calc_random(1)', ()=>{
-    for (i = 0; i < 100; i++) {
-        expect(password_generator.calc_random(1)).toBe(0)
-    }
+    const mock_webcrypto = new mock([[0],[255],[254]]);
+    const password_generator = new pwgenerator(mock_webcrypto);
+    expect(password_generator.calc_random(1)).toBe(0)
+    expect(password_generator.calc_random(1)).toBe(0)
 });
 test('test of calc_random(2)', ()=>{
-    for (i = 0; i < 100; i++) {
-        let random = password_generator.calc_random(2)
-        expect(random).toBeGreaterThanOrEqual(0);
-        expect(random).toBeLessThan(2);
-    }
+    const mock_webcrypto = new mock([[0],[255],[254],[253]]);
+    const password_generator = new pwgenerator(mock_webcrypto);
+    expect(password_generator.calc_random(2)).toBe(0)
+    expect(password_generator.calc_random(2)).toBe(1)
 });
 test('test of calc_random(255)', ()=>{
-    for (i = 0; i < 1000; i++) {
-        let random = password_generator.calc_random(255)
-        expect(random).toBeGreaterThanOrEqual(0);
-        expect(random).toBeLessThan(255);
-    }
+    const mock_webcrypto = new mock([[0],[255],[254]]);
+    const password_generator = new pwgenerator(mock_webcrypto);
+    expect(password_generator.calc_random(255)).toBe(0)
+    expect(password_generator.calc_random(255)).toBe(254)
 });
